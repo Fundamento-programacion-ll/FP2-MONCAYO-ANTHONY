57798658e1d10f6400ae7834c7645d1f008e84af
AnthonyMonc <antho_monca@hotmail.com>
2020-01-20 09:22:21 -0500

    java

---
+++ Ejemplonumerouno/src/MVC/MVC.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MVC;
+
+import modelo.persona;
+
+/**
+ *
+ * @author antho
+ */
+public class MVC {
+    public static void main(String[] args) {
+        
+        persona Kevin= new persona("Kevin", "Jimenez","la 41",1750432113,20,984534324,'m',true);
+        persona Bryan= new persona("Bryan", "Jimenez","la 41",1734654734,54,573727543,'m',true);
+        persona Juan= new persona("Juan", "Jimenez","la 41",1759365625,32,575625457,'m',true);
+    }
+    
+    
+}
