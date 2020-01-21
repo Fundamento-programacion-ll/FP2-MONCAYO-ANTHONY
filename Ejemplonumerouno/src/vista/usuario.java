56def1c1d0587a41fe257f4a2b7c272488a53830
AnthonyMonc <antho_monca@hotmail.com>
2020-01-21 09:18:37 -0500

    mvc

--- Ejemplonumerouno/src/vista/usuario.java
+++ Ejemplonumerouno/src/vista/usuario.java
@@ -5,10 +5,47 @@
  */
 package vista;
 
+import controlador.user;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author antho
  */
 public class usuario {
+    user metodos = new user();
+    
+    public void menu(){
+    
+        int opcion =0;
+        opcion = Integer.parseInt(JOptionPane.showInputDialog(null,"Menu\n"+"1. Agregar persona\n"+"2. Listar personas\n"
+                +"3. Salir\n",this ));
+    do {
+    switch (opcion){
+    
+        case 1 :
+            String nombre = JOptionPane.showInputDialog(null,"Ingrese un nombre");
+            metodos.agregarpersona(nombre);
+            System.out.println(metodos.ListarPersona());
+            break;
+        case 2 :
+            System.out.println("Hola");
+            System.out.println(metodos.ListarPersona());
+            
+            break;
+            
+        case 3 :
+            
+            opcion =3;
+            break;
+            
+        default:
+            opcion =3;
+            
+            break;
+    }
+    }while(opcion !=3);
+    }
+    
     
 }
