e9cc35955fec47862e3c88ff4851c72505c09f33
AnthonyMonc <antho_monca@hotmail.com>
2020-02-03 08:40:22 -0500

    empleado

---
+++ Trabajo_en_clase_empleado/src/trabajo_en_clase_empleado/Empleado_base_mas_comision.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabajo_en_clase_empleado;
+
+/**
+ *
+ * @author antho
+ */
+public class Empleado_base_mas_comision {
+    
+}
---
+++ Trabajo_en_clase_empleado/src/trabajo_en_clase_empleado/Trabajo_en_clase_empleado.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabajo_en_clase_empleado;
+
+/**
+ *
+ * @author antho
+ */
+public class Trabajo_en_clase_empleado {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
