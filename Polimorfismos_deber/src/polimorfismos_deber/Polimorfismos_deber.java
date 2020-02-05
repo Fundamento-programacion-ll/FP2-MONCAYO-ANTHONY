182a82e0cccb5c8b1d5981fd591862c63123f336
AnthonyMonc <antho_monca@hotmail.com>
2020-02-04 21:46:45 -0500

    final

--- Polimorfismos_deber/src/polimorfismos_deber/Polimorfismos_deber.java
+++ Polimorfismos_deber/src/polimorfismos_deber/Polimorfismos_deber.java
@@ -23,12 +23,12 @@ public class Polimorfismos_deber {
         deposito d=new  deposito();
         retiros r=new retiros();
         solictud_saldo s=new solictud_saldo();
-        
-        String [] usuario = new String[3];
-        
-        int [] Ncuenta = new int[3];
-        int [] saldo = new int[3];
-        String [] contraseña = new String[3];
+        String usua,contr;
+        int ncun, sal;
+        String [] usuario = new String[10];
+        int [] Ncuenta = new int[10];
+        int [] saldo = new int[10];
+        String [] contraseña = new String[10];
         // Usuarios
         usuario [0] = "Anthony";
         usuario [1] = "Toñito";
@@ -46,11 +46,11 @@ public class Polimorfismos_deber {
         saldo [1]  = 300;
         saldo [2]  = 300;
         
-        int opc2,opc,nu=0,nnc,nc=0;
+        int opc2,opc,nu=0,nnc,nc=0,sald;
         boolean v=false;
         do{
         do {
-            u=JOptionPane.showInputDialog(null,"Ingrese su usuario", "Banco Del Barrio");
+            u=JOptionPane.showInputDialog(null,"Ingrese su usuario");
             for (int i = 0; i <usuario.length; i++) {
             if (u.equalsIgnoreCase(usuario[i])){
                 nu=i;
@@ -69,30 +69,40 @@ public class Polimorfismos_deber {
             }
          }while(v==false); 
         
-        opc = Integer.parseInt(JOptionPane.showInputDialog(null,"Menu \n1. Deposito saldo \n2. Retiro \n 3. Consultar Saldo"));
+        opc = Integer.parseInt(JOptionPane.showInputDialog(null,"Menu \n1. Deposito saldo \n2. Retiro \n 3. Consultar Saldo  \n 4. Agregar cuenta "));
         switch (opc){
             //Deposito
             case 1:
-                int dep=Integer.parseInt(JOptionPane.showInputDialog(null,"Ingrese su Deposito"));
+                sald=Integer.parseInt(JOptionPane.showInputDialog(null,"Ingrese su numero de cuenta"));
+                for (int i = 0; i <Ncuenta.length; i++) {
+                    if (Nc==Ncuenta[i]){
+                    int dep=Integer.parseInt(JOptionPane.showInputDialog(null,"Ingrese su Deposito"));
                 d.setCuenta(Ncuenta[nu]);
                 d.setMontoDep(dep);
                 
                 d.ejecutar();
                 System.out.println(d);
                 System.out.println("Saldo total: "+ d.dep(dep, saldo[nu]));
+                    }
+                }
                 break;
             //Retiro    
             case 2:
-                
-                int ret=Integer.parseInt(JOptionPane.showInputDialog(null,"Ingrese su Retiro"));
+                sald=Integer.parseInt(JOptionPane.showInputDialog(null,"Ingrese su numero de cuenta"));
+                for (int i = 0; i <Ncuenta.length; i++) {
+                    if (Nc==Ncuenta[i]){
+                    int ret=Integer.parseInt(JOptionPane.showInputDialog(null,"Ingrese su Retiro"));
                 r.setCuenta(Ncuenta[nu]);
                 r.setMonto(ret);
                 r.ejecutar();
                 System.out.println("Saldo total: "+ d.dep(ret, saldo[nu]));
+                    }
+                }
+                
                 break;
             //COnsultar Saldo
             case 3:
-                int sald=Integer.parseInt(JOptionPane.showInputDialog(null,"Ingrese su numero de cuenta"));
+                 sald=Integer.parseInt(JOptionPane.showInputDialog(null,"Ingrese su numero de cuenta"));
                 for (int i = 0; i <Ncuenta.length; i++) {
                     if (Nc==Ncuenta[i]){
                         s.setValor(saldo[i]);
@@ -103,11 +113,29 @@ public class Polimorfismos_deber {
                 }
                 
                 break;
+            case 4:
+                int cont =0;
+                usua=JOptionPane.showInputDialog(null,"Ingrese su Nombre de Usuario");
+                usuario [3+cont]=usua;
+                contr=JOptionPane.showInputDialog(null,"Ingrese su Contraseña");
+                contraseña [3+cont]=contr;
+                ncun=Integer.parseInt(JOptionPane.showInputDialog(null,"Ingrese su numero de cuenta"));
+                Ncuenta [3+cont]=ncun;
+                sal=Integer.parseInt(JOptionPane.showInputDialog(null,"Ingrese su numero Saldo"));
+                saldo [3+cont]=sal;
+                
+                System.out.println("Su cuenta fue creada con exito: \n Usuario: "+ usuario[3+cont]
+                        +" Contraseña: "+ contraseña[3+cont] +" Numero de cuentra: "+ Ncuenta[3+cont]
+                        + " Saldo: "+ saldo[3+cont]);
+                cont++;
+                
+                break;
+            
             default:
+                System.out.println("Gracias por Preferirnos");
                 break;
         }
-        //Stotal= s.getValor()+d.getMontoDep()-r.getMonto();
-        //System.out.println("El saldo total que tiene es de: "+Stotal);
+        
             opc2 = JOptionPane.showConfirmDialog(null, "Desea continuar usando el programa");
         }while(opc2==0);
     }
